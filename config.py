8218b3b9e9ffcf60f6953ae4105451b93f20b27d
Robert Khousadian <rkhousadian@gmail.com>
2018-08-13 12:13:25 -0700

    create config file for upcoming update
    move URL to a config file, bring in options for what computer your using (this will be used for notifying you with a sound when patch notes are available!)

---
+++ config.py
@@ -0,0 +1,4 @@
+# store everything in between the single quotes please!
+
+operating_sytem = '' # this can only be on of the following options: 'macOS', 'linux', or 'windows'
+url = '' # URL for upcoming patch notes! example: https://www.epicgames.com/fortnite/en-US/patch-notes/v5-20
